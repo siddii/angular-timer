23760b4a2b5a6b778a4482bc810ae656c5bd7d2d
Siddique Hameed <siddii+git@gmail.com>
2013-05-29 09:42:11 -0500

    working on unit test

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -3,8 +3,8 @@ basePath = '../';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'app/lib/angular/angular.js',
-  'app/lib/angular/angular-*.js',
+  'angular/angular.js',
+  'angular/angular-*.js',
   'test/lib/angular/angular-mocks.js',
   'app/js/**/*.js',
   'test/unit/**/*.js'
--- test/unit/directivesSpec.js
+++ test/unit/directivesSpec.js
@@ -2,44 +2,26 @@
 
 /* jasmine specs for directives go here */
 
-describe('directives', function() {
-  beforeEach(module('myApp.directives'));
+describe('timer directive', function() {
+  beforeEach(module('timer'));
 
-  describe('app-version', function() {
-    it('should print current version', function() {
-      module(function($provide) {
-        $provide.value('version', 'TEST_VER');
-      });
-      inject(function($compile, $rootScope) {
-        var element = $compile('<span app-version></span>')($rootScope);
-        expect(element.text()).toEqual('TEST_VER');
+  describe('default timer', function() {
+    it('should run timer with 1 millisecond interval', function() {
+
+      inject(function($compile, $rootScope, $browser, $timeout) {
+        var $scope = $rootScope.$new();
+        var element = $compile('<timer/>')($scope);
+        $timeout(function() {
+          console.log('######### $scope = ', element.html());
+          console.log('########## $scope.startTime = ', $scope.startTime);
+        }, 100);
+        $timeout(function() {
+          console.log('######### $scope = ', element.html());
+          console.log('########## $scope.startTime = ', $scope.startTime);
+        }, 200);
+        $browser.defer.flush();
       });
     });
   });
 
-  describe('Unit testing great quotes', function() {
-    var $compile;
-    var $rootScope;
-
-    // Load the myApp module, which contains the directive
-    beforeEach(module('myApp'));
-
-    // Store references to $rootScope and $compile
-    // so they are available to all tests in this describe block
-    beforeEach(inject(function(_$compile_, _$rootScope_){
-      // The injector unwraps the underscores (_) from around the parameter names when matching
-      $compile = _$compile_;
-      $rootScope = _$rootScope_;
-    }));
-
-    it('Replaces the element with the appropriate content', function() {
-      // Compile a piece of HTML containing the directive
-      var element = $compile("<a-great-eye></a-great-eye>")($rootScope);
-      // fire all the watches, so the scope expression {{1 + 1}} will be evaluated
-      $rootScope.$digest();
-      // Check that the compiled element contains the templated content
-      console.log('#######  require = ', require);
-      expect(element.html()).toContain("lidless, wreathed in flame, 2 times");
-    });
-  });
 });
