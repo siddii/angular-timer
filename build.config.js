e07f856bca348ead364931dfefde01b9a6b7bfb4
Siddique Hameed <siddii+git@gmail.com>
2013-07-23 09:32:13 -0500

    using release path

--- build.config.js
+++ build.config.js
@@ -2,12 +2,10 @@
  * This file/module contains all configuration for the build process.
  */
 module.exports = {
- 
-  build_dir: 'build',
-  compile_dir: 'release',
+    build_dir: 'build',
+    compile_dir: 'release',
 
- app_files: {
-    js: [ 'app/**/*.js', '!app/**/*.spec.js' ]
-  },
-
+    app_files: {
+        js: [ 'app/**/*.js', '!app/**/*.spec.js' ]
+    }
 };
