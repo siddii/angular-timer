32881cba11e427fa118d94cfe7a06baa99a27137
Siddique Hameed <siddii+git@gmail.com>
2013-09-23 09:14:07 -0500

    Gruntfile clean up

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function ( grunt ) {
+module.exports = function (grunt) {
 
   /**
    * Load required Grunt tasks. These are installed based on the versions listed
@@ -19,28 +19,20 @@ module.exports = function ( grunt ) {
   /**
    * Load in our build configuration file.
    */
-  var userConfig = require( './build.config.js' );
+  var userConfig = {
+    compile_dir: 'release',
 
-  /**
-   * This is the configuration object Grunt uses to give each plugin its
-   * instructions.
-   */
+    app_files: {
+      js: [ 'app/**/*.js', '!app/**/*.spec.js' ]
+    }
+  };
+
   var taskConfig = {
-    /**
-     * We read in our `package.json` file so we can access the package name and
-     * version. It's already there, so we don't repeat ourselves here.
-     */
     pkg: grunt.file.readJSON("package.json"),
 
-    /**
-     * The banner is the comment that is placed at the top of our compiled
-     * source files. It is first processed as a Grunt template, where the `<%=`
-     * pairs are evaluated based on this very configuration object.
-     */
     meta: {
-      banner:
-        '/**\n' +
-        ' * <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %>\n' +
+      banner: '/**\n' +
+        ' * <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd h:MM TT") %>\n' +
         ' * <%= pkg.homepage %>\n' +
         ' *\n' +
         ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
@@ -48,89 +40,7 @@ module.exports = function ( grunt ) {
         ' */\n'
     },
 
-    /**
-     * Creates a changelog on a new version.
-     */
-    changelog: {
-      options: {
-        dest: 'CHANGELOG.md',
-        template: 'changelog.tpl'
-      }
-    },
-
-    /**
-     * Increments the version number, etc.
-     */
-    bump: {
-      options: {
-        files: [
-          "package.json",
-          "bower.json"
-        ],
-        commit: false,
-        commitMessage: 'chore(release): v%VERSION%',
-        commitFiles: [
-          "package.json",
-          "client/bower.json"
-        ],
-        createTag: false,
-        tagName: 'v%VERSION%',
-        tagMessage: 'Version %VERSION%',
-        push: false,
-        pushTo: 'origin'
-      }
-    },
-
-    /**
-     * The directories to delete when `grunt clean` is executed.
-     */
-    clean: [
-      '<%= build_dir %>',
-      '<%= compile_dir %>'
-    ],
-
-    /**
-     * The `copy` task just copies files from A to B. We use it here to copy
-     * our project assets (images, fonts, etc.) and javascripts into
-     * `build_dir`, and then to copy the assets to `compile_dir`.
-     */
-    copy: {
-      build_appjs: {
-        files: [
-          {
-            src: [ '<%= app_files.js %>' ],
-            dest: '<%= build_dir %>/',
-            cwd: '.',
-            expand: true
-          }
-        ]
-      },
-	copy_appjs: {
-        files: [
-          {
-            src : '<%= build_dir %>/app/js/timer.js',
-            dest: '<%= compile_dir %>/',
-            cwd: '.',
-			filter: 'isFile',
-			flatten: true,
-            expand: true,
-			rename: function(dest, src) {
-              return dest + '<%= pkg.name %>.js';
-            }
-          }
-        ]
-      }
-    },
-
-
-    /**
-     * `grunt concat` concatenates multiple source files into a single file.
-     */
     concat: {
-      /**
-       * The `compile_js` target is the concatenation of our application source
-       * code and all specified vendor source code into a single file.
-       */
       compile_js: {
         options: {
           banner: '<%= meta.banner %>'
@@ -142,9 +52,6 @@ module.exports = function ( grunt ) {
       }
     },
 
-    /**
-     * Minify the sources!
-     */
     uglify: {
       options: {
         banner: '<%= meta.banner %>'
@@ -155,31 +62,6 @@ module.exports = function ( grunt ) {
       }
     },
 
-    /**
-     * `ng-min` annotates the sources before minifying. That is, it allows us
-     * to code without the array syntax.
-     */
-    ngmin: {
-      compile: {
-        files: [
-          {
-            src: [ '<%= app_files.js %>' ],
-            cwd: '<%= build_dir %>',
-            dest: '<%= build_dir %>',
-            expand: true
-          }
-        ]
-      }
-    },
-
-    /**
-     * `jshint` defines the rules of our linter as well as which files we
-     * should check. This file, all javascript sources, and all our unit tests
-     * are linted based on the policies listed in `options`. But we can also
-     * specify exclusionary patterns by prefixing them with an exclamation
-     * point (!); this is useful when code comes from a third party but is
-     * nonetheless inside `src/`.
-     */
     jshint: {
       src: [
         '<%= app_files.js %>'
@@ -197,85 +79,15 @@ module.exports = function ( grunt ) {
         eqnull: true
       },
       globals: {}
-    },
-
-    /**
-     * The `index` task compiles the `index.html` file as a Grunt template. CSS
-     * and JS files co-exist here but they get split apart later.
-     */
-    index: {
-
-      /**
-       * During development, we don't want to have wait for compilation,
-       * concatenation, minification, etc. So to avoid these steps, we simply
-       * add all script files directly to the `<head>` of `index.html`. The
-       * `src` property contains the list of included files.
-       */
-      build: {
-        dir: '<%= build_dir %>',
-        src: [
-          '<%= build_dir %>/src/**/*.js'
-        ]
-      },
-
-      /**
-       * When it is time to have a completely compiled application, we can
-       * alter the above to include only a single JavaScript and a single CSS
-       * file. Now we're back!
-       */
-      compile: {
-        dir: '<%= compile_dir %>',
-        src: [
-          '<%= concat.compile_js.dest %>'
-        ]
-      }
     }
-
   };
 
-  grunt.initConfig( grunt.util._.extend( taskConfig, userConfig ) );
+  grunt.initConfig(grunt.util._.extend(taskConfig, userConfig));
 
-  /**
-   * In order to make it safe to just compile or copy *only* what was changed,
-   * we need to ensure we are starting from a clean, fresh build. So we rename
-   * the `watch` task to `delta` (that's why the configuration var above is
-   * `delta`) and then add a new task called `watch` that does a clean build
-   * before watching for changes.
-   */
-  grunt.renameTask( 'watch', 'delta' );
-  grunt.registerTask( 'watch', [ 'build'] );
+  grunt.registerTask('default', [ 'build']);
 
-  /**
-   * The default task is to build and compile.
-   */
-  grunt.registerTask( 'default', [ 'build', 'compile' ] );
-
-  /**
-   * The `build` task gets your app ready to run for development and testing.
-   */
-  grunt.registerTask( 'build', [
-    'clean', 'jshint', 
-    'copy:build_appjs',
-	'copy:copy_appjs'
-    
+  grunt.registerTask('build', [
+    'jshint', 'concat', 'uglify'
   ]);
-
-  /**
-   * The `compile` task gets your app ready for deployment by concatenating and
-   * minifying your code.
-   */
-  grunt.registerTask( 'compile', [
-    'ngmin', 'concat', 'uglify'
-  ]);
-
-  /**
-   * A utility function to get all app JavaScript sources.
-   */
-  function filterForJS ( files ) {
-    return files.filter( function ( file ) {
-      return file.match( /\.js$/ );
-    });
-  }
-
 
 };
--- build.config.js
+++ build.config.js
@@ -2,7 +2,6 @@
  * This file/module contains all configuration for the build process.
  */
 module.exports = {
-    build_dir: 'build',
     compile_dir: 'release',
 
     app_files: {
