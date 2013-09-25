fc577c85505edbe751ce0a4d1de173458f99bdac
Siddique Hameed <siddii+git@gmail.com>
2013-09-25 09:29:47 -0500

    more refactoring

---
+++ docs/docs.js
@@ -0,0 +1,22 @@
+function startTimer(sectionId) {
+  document.getElementById(sectionId).getElementsByTagName('timer')[0].start();
+}
+
+function stopTimer(sectionId) {
+  document.getElementById(sectionId).getElementsByTagName('timer')[0].stop();
+}
+
+function stopResumeTimer(sectionId, btn) {
+  if (btn.innerHTML === 'Start') {
+    document.getElementById(sectionId).getElementsByTagName('timer')[0].start();
+    btn.innerHTML = 'Stop';
+  }
+  else if (btn.innerHTML === 'Stop') {
+    document.getElementById(sectionId).getElementsByTagName('timer')[0].stop();
+    btn.innerHTML = 'Resume';
+  }
+  else {
+    document.getElementById(sectionId).getElementsByTagName('timer')[0].resume();
+    btn.innerHTML = 'Stop';
+  }
+}
