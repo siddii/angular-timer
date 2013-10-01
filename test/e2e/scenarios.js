febc61601e276b687bef572c1641cce5ee8d0dc9
Siddique Hameed <siddii+git@gmail.com>
2013-10-01 15:52:17 -0500

    WIP - e2e testing

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,27 +1,25 @@
 'use strict';
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
+describe('Angular Timer E2E Tests', function () {
 
-describe('timer directive', function() {
-
-  beforeEach(function() {
-      browser().navigateTo('angular-timer/index.html');
+  beforeEach(function () {
+    browser().navigateTo('/index.html');
   });
 
-  it('should stop when user click on stop button [Simple Timer]', function() {
-      sleep(1);
-      element('#basic-timer button:last-child').click();
-      var oldValue = element('#basic-timer span').text();
-      sleep(1);   
-      expect( element('#basic-timer span').text() ).toBe( oldValue );
-  });
-  
-  it('should be reset when user click on start button again [Simple Timer]', function() {
-      sleep(1);
-      var oldValue = element('#basic-timer span').text();
-      element('#basic-timer button:nth-child(3)').click();
-      element('#basic-timer button:last-child').click();
-      expect( element('#basic-timer span').text() ).toBeLessThan( oldValue );
+  it("Simple Timer - Should stop ticking when user clicks 'Stop' button", function () {
+    sleep(1);
+    element('#basic-timer button:last-child').click();
+    var oldValue = element('#basic-timer span').text();
+    sleep(1);
+    expect( element('#basic-timer span').text() ).toBe( oldValue );
   });
 
+  it('Simple Timer - Should reset timer value when user click on start button again', function () {
+    sleep(1);
+    var oldValue = element('#basic-timer span').text();
+    element('#basic-timer button:nth-child(3)').click();
+    element('#basic-timer button:last-child').click();
+    expect( element('#basic-timer span').text() ).toBeLessThan( oldValue );
+  });
 });
