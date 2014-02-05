8651fd33e5799f1b43886e9e5a4df9874f0264ae
johnparn <john.parnefjord@gmail.com>
2014-02-05 19:32:53 +0100

    Added listener to be able to set countdown value
    With the listener one can set the countdown value using
    $broadcast('countdown-set', value). This might be an edge case, but I
    need to set a new countdown. This makes the countdown value dynamic set.
    Issue #23 describes how to set the value on initialization but using
    this one can change the value whenever necessary.

--- dist/angular-timer.js
+++ dist/angular-timer.js
@@ -52,6 +52,10 @@ angular.module('timer', [])
           $scope.clear();
         });
 
+        $scope.$on('countdown-set', function (e, countdown) {
+          $scope.countdown = countdown;
+        });
+        
         function resetTimeout() {
           if ($scope.timeoutId) {
             clearTimeout($scope.timeoutId);
