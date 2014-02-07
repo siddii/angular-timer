248f4f132b2eabfbe46980a8abb0e8b168ed476d
johnparn <john.parnefjord@gmail.com>
2014-02-07 19:01:16 +0100

    Countdown set function and emit 0 as last value
    With the listener one can set the countdown value using
    $broadcast('countdown-set', value). This might be an edge case, but I
    need to set a new countdown. Issue #23 describes how to set the value on
    initialization but using this one can change the value whenever
    necessary.
    
    Also changed the order of execution of some code to make 0 the last
    emitted millis value for a countdown. In the original code the last emit
    message was the value just
    before 0. For example with a countdown with an interval of 1000 the last
    emitted millis value was 1000.

--- app/js/timer.js
+++ app/js/timer.js
@@ -45,6 +45,10 @@ angular.module('timer', [])
           $scope.clear();
         });
 
+        $scope.$on('timer-set-countdown', function (e, countdown) {
+          $scope.countdown = countdown;
+        });
+
         function resetTimeout() {
           if ($scope.timeoutId) {
             clearTimeout($scope.timeoutId);
@@ -136,13 +140,6 @@ angular.module('timer', [])
             return;
           }
           calculateTimeUnits();
-          if ($scope.countdown > 0) {
-            $scope.countdown--;
-          }
-          else if ($scope.countdown <= 0) {
-            $scope.stop();
-            return;
-          }
 
           //We are not using $timeout for a reason. Please read here - https://github.com/siddii/angular-timer/pull/5
           $scope.timeoutId = setTimeout(function () {
@@ -151,6 +148,15 @@ angular.module('timer', [])
           }, $scope.interval - adjustment);
 
           $scope.$emit('timer-tick', {timeoutId: $scope.timeoutId, millis: $scope.millis});
+
+          if ($scope.countdown > 0) {
+            $scope.countdown--;
+          }
+          else if ($scope.countdown <= 0) {
+            $scope.stop();
+            return;
+          }
+
         };
 
         if ($scope.autoStart === undefined || $scope.autoStart === true) {
