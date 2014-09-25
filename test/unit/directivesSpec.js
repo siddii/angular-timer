bb80f07f761e00084246e4d947915c9c7820e9ba
FranciscoM <fmont006@fiu.edu>
2014-09-24 23:35:46 -0400

    Change order of classed.

--- test/unit/directivesSpec.js
+++ test/unit/directivesSpec.js
@@ -13,7 +13,7 @@ describe('timer directive', function() {
         var element = $compile('<timer/>')($scope);
         $scope.$digest();
         $timeout(function() {
-          expect(element.html()).toMatch(/^<span class="ng-scope ng-binding">/);
+          expect(element.html()).toMatch(/^<span class="ng-binding ng-scope">/);
           expect(element.html().indexOf('</span>')).toBeGreaterThan(-1);
           $rootScope.$broadcast('timer-stop');
         }, 300);
