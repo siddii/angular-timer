f990e4b55c68947038abbf97616f7f0b21d59f8e
Jakub Kočvara <jakubkocvara@gmail.com>
2014-11-05 15:15:53 +0100

    Fix IE countdown

--- app/js/timer.js
+++ app/js/timer.js
@@ -1,3 +1,10 @@
+/**
+ * angular-timer - v1.1.6 - 2014-07-01 7:37 AM
+ * https://github.com/siddii/angular-timer
+ *
+ * Copyright (c) 2014 Siddique Hameed
+ * Licensed MIT <https://github.com/siddii/angular-timer/blob/master/LICENSE.txt>
+ */
 var timerModule = angular.module('timer', [])
   .directive('timer', ['$compile', function ($compile) {
     return  {
@@ -8,11 +15,12 @@ var timerModule = angular.module('timer', [])
         startTimeAttr: '=startTime',
         endTimeAttr: '=endTime',
         countdownattr: '=countdown',
+        finishCallback: '&finishCallback',
         autoStart: '&autoStart',
         maxTimeUnit: '='
       },
       controller: ['$scope', '$element', '$attrs', '$timeout', function ($scope, $element, $attrs, $timeout) {
-
+        console.log($scope.interval);
         // Checking for trim function since IE8 doesn't have it
         // If not a function, create tirm with RegEx to mimic native trim
         if (typeof String.prototype.trim !== 'function') {
@@ -57,7 +65,7 @@ var timerModule = angular.module('timer', [])
         $scope.$on('timer-set-countdown', function (e, countdown) {
           $scope.countdown = countdown;
         });
-        
+
         $scope.$on('timer-set-value', function (e, seconds) {
           if ($scope.countdownattr) {
             $scope.countdown = seconds;
@@ -220,13 +228,16 @@ var timerModule = angular.module('timer', [])
 
 
           if ($scope.countdownattr) {
-            $scope.millis = $scope.countdown * 1000;
+            $scope.millis = Math.round(($scope.countdown * 1000 - $scope.millis) / 1000) * 1000;
           }
 
           if ($scope.millis < 0) {
             $scope.stop();
             $scope.millis = 0;
             calculateTimeUnits();
+            if($scope.finishCallback) {
+              $scope.$eval($scope.finishCallback);
+            }
             return;
           }
           calculateTimeUnits();
@@ -238,13 +249,6 @@ var timerModule = angular.module('timer', [])
           }, $scope.interval - adjustment);
 
           $scope.$emit('timer-tick', {timeoutId: $scope.timeoutId, millis: $scope.millis});
-
-          if ($scope.countdown > 0) {
-            $scope.countdown--;
-          }
-          else if ($scope.countdown <= 0) {
-            $scope.stop();
-          }
         };
 
         if ($scope.autoStart === undefined || $scope.autoStart === true) {
