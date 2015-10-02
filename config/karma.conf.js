55e203d06d61ab7faac9feff368fcc91beefa950
Valentin Moser <moser.valentin@gmail.com>
2015-10-02 18:05:12 +0200

    moment.js official package should be "moment" instead of "momentjs", modified bower, gruntfile & test config to reflect that

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -6,7 +6,7 @@ module.exports = function(config){
 
     files : [
       'bower_components/humanize-duration/humanize-duration.js',
-      'bower_components/momentjs/min/moment-with-locales.js',
+      'bower_components/moment/min/moment-with-locales.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'app/js/*.js',
